5affb65a3e2fe8192269785f1992c5d65c501427
Ariel Silva <ceosilvajr@gmail.com>
2019-08-01 14:59:26 +0800

    Initial commit

---
+++ src/main/kotlin/com/ceosilvajr/springnotification/SpringNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.ceosilvajr.springnotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringNotificationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringNotificationApplication>(*args)
+}
